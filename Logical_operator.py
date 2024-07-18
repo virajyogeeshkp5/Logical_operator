84b435dcbd74df1501aa437cb8db8acb24e1b292
YOGEESH K P <yogeesh793@gmail.com>
2024-07-18 11:05:19 +0530

    Add files via upload

---
+++ Logical_operator.py
@@ -0,0 +1,15 @@
+# Logical operators
+#1
+x=5
+y=10
+if x>3 and y<15:
+    print("Both x and y are with in specified range")
+
+#2
+A = False
+B = True
+
+print(A and  B)
+print(A or B)
+print(A and A)
+print(B and  B)
